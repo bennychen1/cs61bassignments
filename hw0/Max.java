1fb22abe4e7ab38ef84bdc34238db1f26b3d9d0f
Benny Chen <bechen1@berkeley.edu>
2018-08-27 08:59:46 -0700

    Parse command line args

---
+++ hw0/Max.java
@@ -0,0 +1,15 @@
+public class Max {
+	public static void main(String[] args) {
+		int[] nums = new int[args.length]
+		int count;
+		count = 0;
+		while (count <= args.length) {
+			nums[count] = Integer.parseInt(args[count]);
+		}
+		return arrayMax(nums);
+	}
+	/** returns the largest number of an array */
+	private static int arrayMax(int[] nums) {
+
+	}
+}
