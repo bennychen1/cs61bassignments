e25a7636ab06ef46f14fcc825d9c49ea62978700
Benny Chen <bechen1@berkeley.edu>
2018-10-01 18:02:40 -0700

    Implemented TrReader

--- hw3/TrReader/TrReader.java
+++ hw3/TrReader/TrReader.java
@@ -1,5 +1,6 @@
 import java.io.Reader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /** Translating Reader: a stream that is a translation of an
  *  existing reader.
@@ -11,9 +12,35 @@ public class TrReader extends Reader {
      *  corresponding characters in TO.  That is, change occurrences of
      *  FROM.charAt(i) to TO.charAt(i), for all i, leaving other characters
      *  in STR unchanged.  FROM and TO must have the same length. */
-    public TrReader(Reader str, String from, String to) {
+    Reader str;
+    String from;
+    String to;
+    int pos;
+    int index;
+    ArrayList <Character> words = new ArrayList<Character>();
+    public TrReader(Reader str, String from, String to) throws IOException {
+        this.str = str;
+        this.from = from;
+        this.to = to;
+        pos = -2;
         // FILL IN
     }
+    public int read(char[] cbuf, int off, int len) throws IOException{
+        if (pos == -2) {
+            pos = cbuf.length + 1;
+        }
+        for (int index = off, count = 1; count <= len; count += 1, index += 1) {
+            cbuf[index] = (char) str.read();
+            int charIndex = from.indexOf(cbuf[index]);
+            if (charIndex >= 0) {
+                cbuf[index] = to.charAt(charIndex);
+            }
+        }
+        pos = pos - 1;
+        return pos;
+    }
+
+    public void close() {}
 
     // FILL IN
     // NOTE: Until you fill in the right methods, the compiler will
