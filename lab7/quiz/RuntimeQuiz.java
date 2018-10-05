211db16dbbdb333ad471f7712cef02abf8b58216
Benny Chen <bechen1@berkeley.edu>
2018-10-04 19:18:29 -0700

    Submit quiz

--- lab7/quiz/RuntimeQuiz.java
+++ lab7/quiz/RuntimeQuiz.java
@@ -30,16 +30,16 @@ public class RuntimeQuiz {
         }
     }
 
-    public static Asymptotic f2_notation = Asymptotic.BIG_THETA;
-    public static Runtime f2_runtime = Runtime.EXPONENTIAL;
+    public static Asymptotic f2_notation = Asymptotic.BIG_O;
+    public static Runtime f2_runtime = Runtime.QUADRATIC;
     public int f2(int n) {
         if (n <= 1) return n;
         f1(new int[n]);
         return n + n * f2(n - 1) + n * n * f2(1);
     }
 
-    public static Asymptotic f3_notation = Asymptotic.BIG_O;
-    public static Runtime f3_runtime = Runtime.EXPONENTIAL;
+    public static Asymptotic f3_notation = Asymptotic.BIG_THETA;
+    public static Runtime f3_runtime = Runtime.LOG_N;
     /* When f3 is first called, start will be 0 and end will be the length of the array - 1 */
     public int f3(char[] array, int start, int end) {
         if (array.length <= 1 || end <= start) return 1;
