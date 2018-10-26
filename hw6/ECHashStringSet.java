dd4353aeec8c3721636c912273c955c725d1d7ca
Benny Chen <bechen1@berkeley.edu>
2018-10-26 09:54:34 -0700

    Added buckets

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -1,10 +1,18 @@
 // REPLACE THIS STUB WITH THE CORRECT SOLUTION.
 // The current contents of this file are merely to allow things to compile
 // out of the box.
+
+import java.util.LinkedList;
+import java.util.List;
+
 /** A set of String values.
  *  @author
  */
 class ECHashStringSet implements StringSet {
+    public ECHashStringSet(int M) {
+        
+    }
 
 
+    private LinkedList[]_buckets;
 }
