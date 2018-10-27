2c9b8fcfce2a015af692fb8a5d2fbbdeb6f43ae8
Benny Chen <bechen1@berkeley.edu>
2018-10-26 22:18:30 -0700

    Possible hashset

--- hw6/ECHashStringSet.java
+++ hw6/ECHashStringSet.java
@@ -2,17 +2,89 @@
 // The current contents of this file are merely to allow things to compile
 // out of the box.
 
-import java.util.LinkedList;
 import java.util.List;
+import java.util.ArrayList;
 
 /** A set of String values.
  *  @author
  */
 class ECHashStringSet implements StringSet {
-    public ECHashStringSet(int M) {
+    public ECHashStringSet() {
+        _buckets = new ArrayList<ArrayList<String>>(2);
+        numBuckets = 2;
 
+        for (int i = 0; i < numBuckets; i += 1) {
+            ArrayList<String> initial = new ArrayList<String>();
+            _buckets.add(initial);
+        }
     }
 
+    @Override
+    public void put(String s) { ;
+        if ((_added + 1) / numBuckets > 5) {
+            resize();
+        }
 
-    private LinkedList[]_buckets;
+        int indexToPut = s.hashCode() & 0x7fffffff % numBuckets;
+
+        ArrayList<String> bucketAtIndex = _buckets.get(indexToPut);
+
+        if (!(bucketAtIndex.contains(s))) {
+            bucketAtIndex.add(s);
+            _added += 1;
+
+        }
+    }
+
+    @Override
+    public boolean contains(String s) {
+        int indexToSearch = s.hashCode() % numBuckets;
+        return _buckets.get(indexToSearch).contains(s);
+    }
+
+    @Override
+    public List<String> asList() {
+        List<String> result = new ArrayList<String>();
+
+        for (int i = 0; i < _buckets.size(); i += 1) {
+            result.addAll(_buckets.get(i));
+        }
+
+        return result;
+    }
+
+    /**
+    int hashCode(String s) {
+        int hC = 0;
+        for (int i = s.length() - 1, j = 0; i >= 0; i -= 1, j += 1) {
+            char curChar = s.charAt(i);
+            hC += curChar * (31 ^ j);
+        }
+        return hC;
+    } */
+
+    void resize() {
+        while ((_added + 1) / numBuckets > 0.2) {
+            numBuckets += 1;
+        }
+
+        ArrayList<ArrayList<String>> temp = _buckets;
+        ArrayList<ArrayList<String>> expanded = new ArrayList<ArrayList<String>>(numBuckets);
+        _buckets = expanded;
+
+        for (int i = 0; i < numBuckets; i += 1) {
+            ArrayList<String> initial = new ArrayList<String>();
+            _buckets.add(initial);
+        }
+
+        for (int i = 0; i < _buckets.size(); i += 1) {
+            for (int j = 0; j < _buckets.get(i).size(); j += 1) {
+                put(_buckets.get(i).get(j));
+            }
+        }
+    }
+
+    private ArrayList<ArrayList<String>> _buckets;
+    private int numBuckets;
+    private int _added;
 }
